41a05bc91133bf5702a6934c37d0667f90046fc9
Geyson Soares <geyson.soares@outlook.com>
2019-04-21 20:48:49 -0300

    service calculadora implementado, bem como os testes

--- calculadora/src/app/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,9 @@
+/**
+ * Serviço das operações da calculadora.
+ * @author Geyson Soars
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +11,56 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /**
+   * Método que calcular uma operação matemática dado dois números.
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @returns number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number;
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        resultado = this.somar(num1, num2);
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = this.subtrair(num1, num2);
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = this.multiplicar(num1, num2);
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = this.dividir(num1, num2);
+        break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+  }
+
+  subtrair(num1: number, num2: number): number {
+    return num1 - num2;
+  }
+
+  somar(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+  }
+
+  dividir(num1: number, num2: number): number {
+    return num1 / num2;
+  }
+
 }
