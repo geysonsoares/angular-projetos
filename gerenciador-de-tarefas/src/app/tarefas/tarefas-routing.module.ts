9854f45933f14f43c0287d7364369f6ed5de3635
Geyson Soares <geyson.soares@outlook.com>
2019-04-25 00:26:49 -0300

    rotas definidas e importadas para o cadastrar tarefas

--- gerenciador-de-tarefas/src/app/tarefas/tarefas-routing.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas-routing.module.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 export const TarefasRoutes: Routes = [
     {path: 'tarefas', redirectTo: 'tarefas/listar'},
-    {path: 'tarefas/listar', component: ListarTarefaComponent}
+    {path: 'tarefas/listar', component: ListarTarefaComponent},
+    {path: 'tarefas/cadastrar', component: CadastrarTarefaComponent}
 ];
