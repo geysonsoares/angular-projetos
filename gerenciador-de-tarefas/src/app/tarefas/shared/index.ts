a830f4e2c4439d828b77790f564866577a710647
Geyson Soares <geyson.soares@outlook.com>
2019-04-25 00:19:25 -0300

    componente para cadastro de tarefas

--- gerenciador-de-tarefas/src/app/tarefas/shared/index.ts
+++ gerenciador-de-tarefas/src/app/tarefas/shared/index.ts
@@ -2,4 +2,5 @@ export * from './tarefa.model';
 export * from './tarefa.service';
 export * from '../shared';
 export * from '../listar';
+export * from '../cadastrar';
 export * from '../tarefas-routing.module';
--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 @NgModule({
-  declarations: [ListarTarefaComponent],
+  declarations: [ListarTarefaComponent, CadastrarTarefaComponent],
   imports: [
     CommonModule,
     RouterModule,
