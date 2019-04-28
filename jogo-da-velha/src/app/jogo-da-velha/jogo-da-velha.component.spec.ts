bdeda76b5fa95459307b057900099ff8264520b8
Geyson Soares <geyson.soares@outlook.com>
2019-04-28 17:47:13 -0300

    ajustes no testes unitários

--- jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
+++ jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+import { JogoDaVelhaService } from './shared';
 
 describe('JogoDaVelhaComponent', () => {
   let component: JogoDaVelhaComponent;
@@ -8,7 +9,8 @@ describe('JogoDaVelhaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ JogoDaVelhaComponent ]
+      declarations: [ JogoDaVelhaComponent ],
+      providers: [ JogoDaVelhaService ]
     })
     .compileComponents();
   }));
