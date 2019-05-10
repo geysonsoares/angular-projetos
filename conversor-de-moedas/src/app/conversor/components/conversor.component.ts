1416be59fa735b2968571b35662f0b6b89a3239e
Geyson Soares <geyson.soares@outlook.com>
2019-05-09 23:18:43 -0300

    implementação do componente ts e html

--- conversor-de-moedas/src/app/conversor/components/conversor.component.ts
+++ conversor-de-moedas/src/app/conversor/components/conversor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Moeda, Conversao, ConversaoResponse } from '../models';
+import { MoedaService, ConversorService } from '../services';
 
 @Component({
   selector: 'app-conversor',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversorComponent implements OnInit {
 
-  constructor() { }
+  private moedas: Moeda[];
+  private conversao: Conversao;
+  private possuiErro: boolean;
+  private conversaoResponse: ConversaoResponse;
+
+  @ViewChild('conversaoForm')
+  conversaoForm: NgForm;
+
+  constructor(
+    private conversorService: ConversorService,
+    private moedaService: MoedaService
+  ) { }
 
   ngOnInit() {
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
   }
+
+  init() {
+    this.conversao = new Conversao('BRL', 'USD', null);
+    this.possuiErro = false;
+  }
+
+  converter(): void {
+    if (this.conversaoForm.form.valid) {
+      this.conversorService
+        .converter(this.conversao)
+        .subscribe(
+          res => this.conversaoResponse = res,
+          error => this.possuiErro = true
+        );
+    }
+  }
+
+  
 
 }
--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components';
 import { MoedaService } from './services';
@@ -13,7 +14,8 @@ import { ConversorService } from './services';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     ConversorComponent
