f9708ce0351fdb57a8db910c3860a8d4ddd88fd2
Geyson Soares <geyson.soares@outlook.com>
2019-05-26 20:16:55 -0300

    projeto dashboard criado

---
+++ dashboard/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
